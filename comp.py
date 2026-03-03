bef93f83f47d955db96a273781648d1e45f1ba6a
Ivson <ivsonricardo1@gmail.com>
2026-03-03 19:49:50 -0300

    Correções

--- comp.py
+++ comp.py
@@ -15,4 +15,12 @@ def transformacao(df_vendas, df_gerentes, low_memory=False):
     df_vendas = df_vendas.dropna() # === Excluir linhas com valores nulos
     df_vendas['DATA'] = pd.to_datetime(df_vendas['DATA'], format="%Y-%m-%d").dt.date # === Formatação de data e remoção da hora, pois não há o horário no banco de dados de extração
     df_vendas = df_vendas.drop_duplicates(subset=['ID'], keep='first') # === Remove linhas duplicadas da coluna "ID" mantendo o primeiro registro ([keep='False'] remove todas as duplicatas e [keep='last'] mantém a última)
+    df_vendas['LOJA'] = df_vendas['LOJA'].str.strip() # Padroniza formatação dos nomes das lojas retirando espaços antes e depois do nome da loja
+    df_vendas['LOJA'] = df_vendas['LOJA'].str.title() # "                                      " deixando a inicial de cada palavra em maiúsculo (ex.: "São Paulo" e não "'São paulo' ou 'são paulo'")
     return df_vendas, df_gerentes
+    
+    
+    
+    
+    df_gerentes["Loja"] = df_gerentes["Loja"].str.strip()
+    df_gerentes["Loja"] = df_gerentes["Loja"].str.title()
